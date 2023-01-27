292887d82a186d990ee52b8698408a5ab73f42e9
Leandro <leandrolopez131982@gmail.com>
2023-01-27 08:51:00 -0300

    clase 1

---
+++ main.js
@@ -0,0 +1,21 @@
+/*let numero = 34
+let numero2 = 35.6
+
+let string = "esto es una cadena de caracters";
+let string2 = 'pepe';
+
+var booleanos = true;
+let booleanos2 = false;
+
+let indefinido = undefined;
+*/
+let objeto = {nombre:"jorge", edad: 34, prefecion: "programador"};
+
+let array = [3,4,56,6,"pepe", true];
+let nombre = prompt("ingrese su nombre");
+alert(nombre);
+let anioNacimiento = prompt("ingrese su año de nacimiento");
+let anioActual = 2022;
+let edad = anioActual - anioNacimiento;
+alert("su edad es: " + edad); 
+
